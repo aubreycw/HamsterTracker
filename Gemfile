373207d278391cfd694620d133776043ebece9db
emilycw <emily.rose.cw@gmail.com>
2015-08-10 20:44:53 -0700

    fix typo

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem 'annotate'
 
 gem 'bcrypt'
 
+
+gem 'responders'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/Api/tracking_subject_controller.rb
+++ app/controllers/Api/tracking_subject_controller.rb
@@ -35,5 +35,3 @@ class Api::TrackingSubjectController < ApplicationController
     params.require(:tracking_subject).permit(:title, :body)
   end
 end
-end
-
