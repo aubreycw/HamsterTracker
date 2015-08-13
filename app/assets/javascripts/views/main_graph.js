f3490623a157275d941157212220f53f4ec1a3b2
emilycw <emily.rose.cw@gmail.com>
2015-08-13 11:01:27 -0700

    Improve graphing

--- app/assets/javascripts/views/main_graph.js
+++ app/assets/javascripts/views/main_graph.js
@@ -14,6 +14,15 @@ HamsterTracker.Views.MainGraph = Backbone.CompositeView.extend({
 
   nameSpace: "http://www.w3.org/2000/svg",
 
+// ---------------------------------- Setup ------------------------
+
+  getData: function(){
+    
+  }
+
+
+
+// ---------------------------------- Render -----------------------
   render: function(){
     var svg = d3.select(this.el);
     var dataset = [
@@ -29,28 +38,48 @@ HamsterTracker.Views.MainGraph = Backbone.CompositeView.extend({
                   [ 220,   88 ]
               ];
 
-    // var xscale = d3.scale.linear();
-    // .domain([0,500])
-    // .range([0,400]);
+    var padding = 30
 
-    // var xscale = d3.scale.linear();
-    // .domain([0,500])
-    // .range([0,400]);
+    var xscale = d3.scale.linear()
+    .domain([0,500])
+    .range([padding,600-padding]);
+
+    var yscale = d3.scale.linear()
+    .domain([0,100])
+    .range([400 - padding, padding]);
+
+    var xAxis = d3.svg.axis()
+    .scale(xscale)
+    .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+    .scale(yscale)
+    .orient("left");
 
     svg.selectAll("circle")
       .data(dataset)
       .enter()
       .append("circle")
       .attr("cx", function(d) {
-        return d[0];
+        return xscale(d[0]);
       })
       .attr("cy", function(d) {
-        return d[1];
+        return yscale(d[1]);
       })
       .attr("r", function(d){
-        return d[1]*d[0]*0.001;
+        return d[1]*0.5;
       });
 
+    svg.append("g")
+      .attr("transform", "translate(0,"+(400-padding)+")")
+      .attr("class", "axis")
+      .call(xAxis)
+    ;
+
+    svg.append("g")
+      .attr("class", "axis")
+      .attr("transform", "translate(" + padding + ",0)")
+      .call(yAxis);
     return this;
   },
 
