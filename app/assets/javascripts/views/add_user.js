bd173ae04e41db2f38fccac28e175b124bc51590
emilycw <emily.rose.cw@gmail.com>
2015-08-17 14:59:07 -0700

    Remove extraneous debuggers

--- app/assets/javascripts/views/add_user.js
+++ app/assets/javascripts/views/add_user.js
@@ -22,21 +22,17 @@ HamsterTracker.Views.AddUser = Backbone.View.extend({
 
   submitUser: function (event){
     event.preventDefault();
-    debugger;
     var model = new HamsterTracker.Models.SharedSubject({
         trackingSubjectId: this.tracking_subject_id
       });
     var attrs = this.$el.find("form").serializeJSON();
-    debugger;
     attrs["shared_subject"]["tracking_subject_id"] = this.tracking_subject_id;
     var that = this;
     model.save(attrs, {
       success: function(){
-        debugger;
         Backbone.history.navigate('#/tracking_subjects/'+that.tracking_subject_id, { trigger: true });
       },
       error: function(model, response){
-        debugger;
          $('.errors').empty();
         var $li = $('<li>' + response.responseText + '</li>');
         $('.errors').append($li);
