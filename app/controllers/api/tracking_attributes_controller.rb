e5745e8a43e1ceaaead0fb60de491fd9f4cc5cf5
emilycw <emily.rose.cw@gmail.com>
2015-08-18 16:36:42 -0700

    add checks for user access on tracking attributes controller

--- app/controllers/api/tracking_attributes_controller.rb
+++ app/controllers/api/tracking_attributes_controller.rb
@@ -1,6 +1,9 @@
 class Api::TrackingAttributesController < ApplicationController
   respond_to :json
 
+  before_filter :require_has_read_access!, only: [:show, :index]
+  before_filter :require_has_write_access!, only: [:destroy, :update]
+
   def show
     @tracking_attribute = TrackingAttribute.find(params[:id])
     render :json => @tracking_attribute
@@ -35,6 +38,30 @@ class Api::TrackingAttributesController < ApplicationController
       render :json => @tracking_attribute
     else
       render :json => @tracking_attribute.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def require_has_read_access!
+    @tracking_subject = TrackingSubject.find(params[:tracking_subject_id])
+    return true if @tracking_subject.is_public?
+
+    @shared_subject = SharedSubject.find_by({tracking_subject_id: params[:tracking_subject_id], user_id: current_user.id})
+
+    return true if !!@shared_subject
+
+    unless current_user && current_user.id == @tracking_subject.user_id
+      render :json => "User does not have permission to access this", status: :unauthorized
+    end
+  end
+
+  def require_has_write_access!
+    @shared_subject = SharedSubject.find_by({tracking_subject_id: params[:tracking_subject_id], user_id: current_user.id})
+
+    return true if !!@shared_subject and @shared_subject.has_write_access?
+
+    @tracking_subject = TrackingSubject.find(params[:id])
+    unless current_user && current_user.id == @tracking_subject.user_id
+      render :json => "User does not have permission to access this", status: :unauthorized
     end
   end
 
